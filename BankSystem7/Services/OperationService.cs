4251a1b5033756165e0f9e9e612a531c76a16e9a
mxdnxght <cloudyfg@gmail.com>
2023-04-03 23:15:06 +0300

    initial commit

--- BankSystem7/Services/OperationService.cs
+++ BankSystem7/Services/OperationService.cs
@@ -1,5 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-using MongoDB.Driver;
+﻿using MongoDB.Driver;
 
 namespace BankSystem7.Services;
 
@@ -13,16 +12,6 @@ internal sealed class OperationService<T> where T : class
     public const string DefaultDatabaseName = "Test";
     public const string DefaultCollectionName = "Operations";
     
-    [Obsolete("This constructor contains logic which now in development.")]
-    public OperationService(bool str)
-    {
-        OperationOptionsBuilder<T>.Options.Name = "BankSystem";
-        _settings = MongoClientSettings.FromConnectionString(OperationOptionsBuilder<T>.Options.Connection);
-        _client = new MongoClient(_settings);
-        _database = _client.GetDatabase(OperationOptionsBuilder<T>.Options.DatabaseName);
-        Collection = _database.GetCollection<T>(OperationOptionsBuilder<T>.Options.CollectionName);
-    }
-    
     public OperationService(string databaseName)
     {
         _settings = MongoClientSettings.FromConnectionString(DefaultConnection);
@@ -34,59 +23,7 @@ internal sealed class OperationService<T> where T : class
     {
         _settings = MongoClientSettings.FromConnectionString(DefaultConnection);
         _client = new MongoClient(_settings);
-        _database = _client.GetDatabase(DefaultCollectionName);
+        _database = _client.GetDatabase(DefaultDatabaseName);
         Collection = _database.GetCollection<T>(DefaultCollectionName);
-    }
-
-    // TODO: implement OperationOptionsBuilder
-    // public OperationService(string optionsName)
-    // {
-    //     OperationOptionsBuilder<T>.Options.Name = optionsName;
-    //     _settings = MongoClientSettings.FromConnectionString(OperationOptionsBuilder<T>.Options.Connection);
-    //     _client = new MongoClient(_settings);
-    //     _database = _client.GetDatabase(OperationOptionsBuilder<T>.Options.DatabaseName);
-    //     Collection = _database.GetCollection<T>(OperationOptionsBuilder<T>.Options.CollectionName);
-    //     
-    // }
-    
-    // public OperationService(string connection, string nameDatabase)
-    // {
-    //     _settings = MongoClientSettings.FromConnectionString(connection);
-    //     _client = new MongoClient(_settings);
-    //     _database = _client.GetDatabase(nameDatabase);
-    //     Collection = _database.GetCollection<T>($"{typeof(Credit)}s");
-    // }
-    //
-    // public OperationService(OperationOptionsBuilder<T> optionsBuilder)
-    // {
-    //     Initialize(optionsBuilder);
-    // }
-    //
-    // protected OperationService<T> Initialize(OperationOptionsBuilder<T> optionsBuilder)
-    // {
-    //     OnConfiguring(optionsBuilder);
-    //     _settings = MongoClientSettings.FromConnectionString(OperationOptionsBuilder<T>.Options.Connection);
-    //     _client = new MongoClient(_settings);
-    //     _database = _client.GetDatabase(OperationOptionsBuilder<T>.Options.DatabaseName);
-    //     Collection = _database.GetCollection<T>(OperationOptionsBuilder<T>.Options.CollectionName);
-    //     return this;
-    // }
-    // protected virtual void OnConfiguring(OperationOptionsBuilder<T> optionsBuilder)
-    // {
-    //     optionsBuilder.Set(optionsBuilder);
-    // }
-}
-
-internal sealed class OperationOptionsBuilder<T> where T : class
-{
-    [Required] public string Name { get; set; } = "Default";
-    public string Connection { get; set; } = OperationService<T>.DefaultConnection;
-    public string DatabaseName { get; set; } = "";
-    public string CollectionName { get; set; } = "";
-    public static OperationOptionsBuilder<T> Options { get; private set; } = new ();
-
-    internal void Set(OperationOptionsBuilder<T> options)
-    {
-        Options = options;
     }
 }
