25dca65d8ad62897be92afbc873ec3294fc3d883
mxdnxght <cloudyfg@gmail.com>
2023-04-03 23:55:53 +0300

    added new model Report.cs

---
+++ BankSystem7/Models/Report.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+
+namespace BankSystem7.Models;
+
+public class Report
+{
+    public int Id { get; set; }
+    public DateTime ReportDate { get; set; }
+    public string OperationName { get; set; }
+    public ExceptionModel ExceptionModel { get; set; } = ExceptionModel.Successfully;
+}
+
+public class GeneralReport<T> : UserReport<T> where T : Enum
+{
+    public string MethodName { get; set; }
+    public Assembly Assembly { get; set; }
+}
+
+public class UserReport<T> : Report where T : Enum
+{
+    public T OperationType { get; set; }
+}
+
+public enum OperationType
+{
+    Create = 1,
+    Read,
+    Update,
+    Delete,
+    Exists,
+    Other,
+}
+
+public enum UserOperationType
+{
+    TransferMoney = 1,
+    TakeCredit,
+    PayCredit,
+    RepayCredit
+}
+
