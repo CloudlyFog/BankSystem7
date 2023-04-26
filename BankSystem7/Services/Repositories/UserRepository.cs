c47f00dadf0a699d4e9bd81fc311355db25c5dee
mxdnxght <cloudyfg@gmail.com>
2023-04-26 21:35:19 +0300

    Removed unnecessary compiled query instance.

--- BankSystem7/Services/Repositories/UserRepository.cs
+++ BankSystem7/Services/Repositories/UserRepository.cs
@@ -2,7 +2,6 @@
 using BankSystem7.Models;
 using BankSystem7.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
-using System.Linq;
 using System.Linq.Expressions;
 
 namespace BankSystem7.Services.Repositories;
@@ -19,15 +18,6 @@ public sealed class UserRepository<TUser, TCard, TBankAccount, TBank, TCredit> :
     private BankRepository<TUser, TCard, TBankAccount, TBank, TCredit> _bankRepository;
     private CardRepository<TUser, TCard, TBankAccount, TBank, TCredit> _cardRepository;
     private bool _disposed;
-
-    private readonly Func<ApplicationContext<TUser, TCard, TBankAccount, TBank, TCredit>, LambdaExpression, TUser?> GetEntity =
-        EF.CompileQuery(
-            (ApplicationContext<TUser, TCard, TBankAccount, TBank, TCredit> context, LambdaExpression predicate) =>
-            context.Users
-            .Include(x => x.Card)
-            .ThenInclude(x => x.BankAccount)
-            .ThenInclude(x => x.Bank)
-            .AsNoTracking().FirstOrDefault((Expression<Func<TUser, bool>>)predicate));
 
     public UserRepository()
     {
@@ -133,24 +123,6 @@ public sealed class UserRepository<TUser, TCard, TBankAccount, TBank, TCredit> :
     public TUser Get(Expression<Func<TUser, bool>> predicate)
     {
         return All.FirstOrDefault(predicate) ?? (TUser)User.Default;
-    }
-
-    public TUser GetCompiled(Expression<Func<User, bool>> predicate)
-    {
-        try
-        {
-            var parameters = new ParameterExpression[]
-            {
-                Expression.Parameter(typeof(TUser)),
-            };
-            var expression = Expression.Lambda<Func<TUser, bool>>(predicate, parameters);
-            return GetEntity(_applicationContext, expression) ?? (TUser)User.Default;
-        }
-        catch (Exception e)
-        {
-
-            throw;
-        }
     }
 
     public ExceptionModel Update(TUser item)
