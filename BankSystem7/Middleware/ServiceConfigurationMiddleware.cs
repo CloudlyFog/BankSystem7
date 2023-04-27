13bd8291fdd192b5c79e07dca5f8ccd3afbae338
mxdnxght <cloudyfg@gmail.com>
2023-04-27 20:59:08 +0300

    Created new interface that describe logic of ServiceConfiguration and changed logic of ServiceConfiguration for it. Changed properties accessors to read-only in ServiceConfiguration.cs.

--- BankSystem7/Middleware/ServiceConfigurationMiddleware.cs
+++ BankSystem7/Middleware/ServiceConfigurationMiddleware.cs
@@ -1,7 +1,10 @@
 ﻿using BankSystem7.Models;
 using BankSystem7.Services.Configuration;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace BankSystem7.Middleware;
 
@@ -17,6 +20,7 @@ public sealed class ServiceConfigurationMiddleware<TUser, TCard, TBankAccount, T
     where TCredit : Credit
 {
     private readonly RequestDelegate _next;
+    public static ServiceConfiguration<TUser, TCard, TBankAccount, TBank, TCredit>? ServiceConfiguration { get; private set; }
 
     public ServiceConfigurationMiddleware(RequestDelegate next, ConfigurationOptions options) : base(next, options)
     {
@@ -26,6 +30,21 @@ public sealed class ServiceConfigurationMiddleware<TUser, TCard, TBankAccount, T
 
     public async Task InvokeAsync(HttpContext context)
     {
-        await _next.Invoke(context);
+        ServiceConfiguration = CreateInstance(Options);
+        await context.Response.WriteAsync("Inside of new custom middleware");
+        await _next(context); 
+    }
+}
+
+public static class ServiceConfigurationMiddlewareExtensions
+{
+    public static IApplicationBuilder UseNationBankSystem<TUser, TCard, TBankAccount, TBank, TCredit>(this IApplicationBuilder builder, Action<ConfigurationOptions> options) 
+        where TUser : User
+        where TCard : Card
+        where TBankAccount : BankAccount
+        where TBank : Bank
+        where TCredit : Credit
+    {
+        return builder.UseMiddleware<ServiceConfigurationMiddleware<TUser, TCard, TBankAccount, TBank, TCredit>>(options);
     }
 }
