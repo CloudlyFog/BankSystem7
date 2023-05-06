67e600a84a420abd732a3c160593f2c45d29d1f7
mxdnxght <cloudyfg@gmail.com>
2023-05-07 00:21:28 +0300

    Implemented service for updating setting.

--- BankSystem7/Services/Interfaces/DbContextInitializer.cs
+++ BankSystem7/Services/Interfaces/DbContextInitializer.cs
@@ -1,4 +1,5 @@
-﻿using BankSystem7.Services.Configuration;
+﻿using BankSystem7.Models;
+using BankSystem7.Services.Configuration;
 using Microsoft.EntityFrameworkCore;
 
 namespace BankSystem7.Services.Interfaces;
@@ -9,5 +10,43 @@ public abstract class DbContextInitializer
     {
         foreach (var context in contexts)
             context.Key.GetType()?.GetConstructor(new [] { context.Value.GetType() })?.Invoke(new object[] { context.Value });
+    }
+}
+
+public abstract class OptionsUpdater<TUser, TCard, TBankAccount, TBank, TCredit> : DbContextInitializer
+    where TUser : User
+    where TCard : Card
+    where TBankAccount : BankAccount
+    where TBank : Bank
+    where TCredit : Credit 
+{
+    protected virtual void UpdateOptions(ConfigurationOptions options)
+    {
+        UpdateEnsureOperations(options.EnsureCreated, options.EnsureDeleted);
+        UpdateOnModelCreating(options.Contexts);
+    }
+
+    protected void UpdateConnection(string connection)
+    {
+        ServiceConfiguration<TUser, TCard, TBankAccount, TBank, TCredit>.SetConnection(connection);
+    }
+
+    protected void UpdateDatabaseName(string databaseName)
+    {
+        ServiceConfiguration<TUser, TCard, TBankAccount, TBank, TCredit>.SetConnection(databaseName: databaseName);
+    }
+
+    protected void UpdateEnsureOperations(bool ensureCreated, bool ensureDeleted)
+    {
+        BankServicesOptions<TUser, TCard, TBankAccount, TBank, TCredit>.EnsureCreated = ensureCreated;
+        BankServicesOptions<TUser, TCard, TBankAccount, TBank, TCredit>.EnsureDeleted = ensureDeleted;
+    }
+
+    protected void UpdateOnModelCreating(Dictionary<DbContext, ModelConfiguration>? contexts)
+    {
+        if (contexts is null)
+            throw new ArgumentNullException(nameof(contexts));
+
+        InitializeDbContexts(contexts);
     }
 }
