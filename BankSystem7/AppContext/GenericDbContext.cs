c0cbd6aa1cf5b6a0aa4f12541200af7691a6db7b
mxdnxght <cloudyfg@gmail.com>
2023-05-06 23:25:55 +0300

    Added method that ensures avoiding changes of nested entities. Also updated descriptions of methods.

--- BankSystem7/AppContext/GenericDbContext.cs
+++ BankSystem7/AppContext/GenericDbContext.cs
@@ -69,10 +69,34 @@ public class GenericDbContext<TUser, TCard, TBankAccount, TBank, TCredit> : DbCo
         BankServicesOptions<TUser, TCard, TBankAccount, TBank, TCredit>.InitializeAccess = false;
     }
     
+    /// <summary>
+    /// Method updates states of entity. You should use this method with method <see cref="AvoidChanges"/> for the best state tracking of entities
+    /// </summary>
+    /// <param name="item">entity whose state will be changed</param>
+    /// <param name="state">future state of <see cref="item"/></param>
+    /// <param name="action">action that will invoked after state of entity will be changed. Usually as action you should use method <see cref="AvoidChanges"/></param>
+    /// <param name="context">context that will handle state changing</param>
+    /// <typeparam name="T">type of <see cref="item"/></typeparam>
     public void UpdateTracker<T>(T item, EntityState state, Action action, DbContext context)
     {
         context.ChangeTracker.Clear();
         context.Entry(item).State = state;
         action.Invoke();
+    }
+
+    /// <summary>
+    /// Method ensures that passed entities won't be changed during call method SaveChanges() 
+    /// </summary>
+    /// <param name="entities">entities that shouldn't be changed</param>
+    public void AvoidChanges(object[]? entities)
+    {
+        if (entities is null || entities.Length == 0)
+            return;
+        
+        foreach (var entity in entities)
+        {
+            if (entity is not null)
+                Entry(entity).State = EntityState.Unchanged;
+        }
     }
 }
