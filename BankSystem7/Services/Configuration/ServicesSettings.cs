53c33352e21e6b4fb6bbec239046ca32703117a2
mxdnxght <cloudyfg@gmail.com>
2023-05-21 16:27:35 +0300

    Added configuring connection during runtime.

--- BankSystem7/Services/Configuration/ServicesSettings.cs
+++ BankSystem7/Services/Configuration/ServicesSettings.cs
@@ -1,18 +1,25 @@
-﻿namespace BankSystem7.Services.Configuration;
+﻿using BankSystem7.Models.Connection;
+using BankSystem7.Models.Credentials;
+
+namespace BankSystem7.Services.Configuration;
 
 internal static class ServicesSettings
 {
-    public const string DefaultConnection = @"Server=localhost\\SQLEXPRESS;Data Source=maxim;Initial Catalog=Test;
+    public const string DefaultMicrosoftSqlServerConnection = @"Server=localhost\\SQLEXPRESS;Data Source=maxim;Initial Catalog=Test;
             Integrated Security=True;Persist Security Info=False;Pooling=False;
             MultipleActiveResultSets=False; Encrypt=False;TrustServerCertificate=False";
 
     public const string DefaultDataSource = "maxim";
-    private const string DefaultDatabaseName = "Test";
+    public const string DefaultDatabaseName = "Test";
+    public const string DefaultHost = "localhost";
+    public const string DefaultPort = "5432";
+    public const string DefaultServer = "localhost\\SQLEXPRESS";
     public static bool EnsureDeleted { get; set; }
     public static bool EnsureCreated { get; set; } = true;
-    public static string? Connection { get; set; }
+    public static string? Connection { get; private set; }
     public static bool Ensured { get; set; }
-    internal static bool InitializeAccess { get; set; }
+    public static bool InitializeAccess { get; set; }
+    public static DatabaseManagementSystemType DatabaseManagementSystemType { get; set; } 
 
     public static void SetConnection(string? connection = null, string? databaseName = DefaultDatabaseName, string? dataSource = DefaultDataSource)
     {
@@ -24,12 +31,86 @@ internal static class ServicesSettings
 
         if (databaseName is null || dataSource is null)
         {
-            Connection = DefaultConnection;
+            Connection = DefaultMicrosoftSqlServerConnection;
             return;
         }
 
-        Connection = @$"Server=localhost\\SQLEXPRESS;Data Source={dataSource};Initial Catalog={databaseName};
-            Integrated Security=True;Persist Security Info=False;Pooling=False;
-            MultipleActiveResultSets=False; Encrypt=False;TrustServerCertificate=False";
+        Connection = GetConnectionByDbmsType(DatabaseManagementSystemType, databaseName, dataSource);
+    }
+
+    public static void SetConnection(DatabaseManagementSystemType type,
+        ConnectionConfigurationBase connectionConfiguration, CredentialsBase credentials)
+    {
+        if (connectionConfiguration.Connection is not null && connectionConfiguration.Connection != string.Empty)
+        {
+            Connection = connectionConfiguration.Connection;
+            return;
+        }
+
+        if (connectionConfiguration.DatabaseName is null)
+        {
+            Connection = DefaultMicrosoftSqlServerConnection;
+            return;
+        }
+
+        Connection = GetConnectionByDbmsType(type, connectionConfiguration, credentials);
+    }
+
+    private static string GetConnectionByDbmsType(DatabaseManagementSystemType type, string? databaseName = DefaultDatabaseName, string? dataSource = DefaultDataSource)
+    {
+        var connectionString = type switch
+        {
+            DatabaseManagementSystemType.MicrosoftSqlServer =>
+                @$"Server=localhost\\SQLEXPRESS;Data Source={dataSource};Initial Catalog={databaseName};
+                Integrated Security=True;Persist Security Info=False;Pooling=False;
+                MultipleActiveResultSets=False; Encrypt=False;TrustServerCertificate=False",
+            DatabaseManagementSystemType.PostgreSql => $"postgres://YourUserName:YourPassword@{dataSource}:5433/{databaseName}",
+            DatabaseManagementSystemType.MySql => "",
+            _ => DefaultMicrosoftSqlServerConnection
+        };
+
+        return connectionString;
+    }
+
+    private static string GetConnectionByDbmsType(DatabaseManagementSystemType type,
+        ConnectionConfigurationBase connectionConfiguration, CredentialsBase credentials)
+    {
+        switch (type)
+        {
+            case DatabaseManagementSystemType.MicrosoftSqlServer:
+            {
+                var microsoftCredentials = (MicrosoftCredentials)credentials;
+                var microsoftConnectionConfiguration = (MicrosoftConnectionConfiguration)connectionConfiguration;
+                Connection =
+                    @$"Server={microsoftConnectionConfiguration.Server};Data Source={microsoftConnectionConfiguration.DataSource};
+                        InitialCatalog={microsoftConnectionConfiguration.DatabaseName};Integrated Security={microsoftConnectionConfiguration.IntegratedSecurity}
+                        Persist Security Info={microsoftConnectionConfiguration.PersistSecurityInfo};Pooling={microsoftConnectionConfiguration.Pooling}
+                        Encrypt={microsoftConnectionConfiguration.Encrypt};TrustServerCertificate={microsoftConnectionConfiguration.TrustServerCertificate}
+                        User id={microsoftCredentials.Username};Password={microsoftCredentials.Password}";
+                break;
+            }
+            case DatabaseManagementSystemType.PostgreSql:
+            {
+                var npgsqlCredentials = (NpgsqlCredentials)credentials;
+                var npgsqlConnectionConfiguration = (NpgsqlConnectionConfiguration)connectionConfiguration;
+                Connection =
+                    $"postgres://{npgsqlCredentials.Username}:{npgsqlCredentials.Password}@{npgsqlConnectionConfiguration.Host}:{npgsqlConnectionConfiguration.Port}/{npgsqlConnectionConfiguration.DatabaseName}";
+                Connection =
+                    $"Server={npgsqlConnectionConfiguration.Host};Port={npgsqlConnectionConfiguration.Port};Database={npgsqlConnectionConfiguration.DatabaseName};User Id={npgsqlCredentials.Username};Password={npgsqlCredentials.Password}";
+                break;
+            }
+            case DatabaseManagementSystemType.MySql:
+            {
+                Connection = $"";
+                break;
+            }
+            default:
+            {
+                Connection = DefaultMicrosoftSqlServerConnection;
+                break;
+            }
+        }
+
+        return Connection;
     }
 }
