6cdafa194761f9fa73a39f3f6e60598f70c72102
mxdnxght <cloudyfg@gmail.com>
2023-06-20 00:36:47 +0300

    Cleared files by CodeMaid.

--- BankSystem7/Services/Interfaces/Base/IBase.cs
+++ BankSystem7/Services/Interfaces/Base/IBase.cs
@@ -1,12 +1,6 @@
 ﻿using BankSystem7.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BankSystem7.Services.Interfaces.Base;
-
 
 public interface IUserRepository<TUser> : IRepository<TUser>, IRepositoryAsync<TUser> where TUser : User
 {
