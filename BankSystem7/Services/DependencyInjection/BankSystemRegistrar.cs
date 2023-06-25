4471ce94fbd82d618330d9e9407e619082c570d8
mxdnxght <cloudyfg@gmail.com>
2023-06-25 15:03:39 +0300

    Added catching exceptions in method Inject and its overloads.

--- BankSystem7/Services/DependencyInjection/BankSystemRegistrar.cs
+++ BankSystem7/Services/DependencyInjection/BankSystemRegistrar.cs
@@ -26,8 +26,7 @@ public sealed class BankSystemRegistrar
 
     public static ServiceProvider Inject(Type[] dependenciesTypes, object[] dependencies)
     {
-        if (dependencies.Length != dependenciesTypes.Length)
-            throw new InvalidOperationException();
+        CheckLength(dependenciesTypes, dependencies);
 
         var services = new ServiceCollection();
         for (int i = 0; i < dependencies.Length; i++)
@@ -38,13 +37,19 @@ public sealed class BankSystemRegistrar
 
     public static ServiceProvider Inject(Type[] dependenciesTypes, IDependencyInjectionRegistrar[] dependencies)
     {
-        if (dependencies.Length != dependenciesTypes.Length)
-            throw new InvalidOperationException();
+        CheckLength(dependenciesTypes, dependencies);
 
         var services = new ServiceCollection();
         for (int i = 0; i < dependencies.Length; i++)
             services.AddSingleton(dependenciesTypes[i], dependencies[i]);
 
         return services.BuildServiceProvider();
+    }
+
+    private static void CheckLength(Type[] dependenciesTypes, object[] dependencies)
+    {
+        if (dependencies.Length != dependenciesTypes.Length)
+            throw new InvalidOperationException($"Length of {nameof(dependenciesTypes)} and {nameof(dependencies)} is different. " +
+                $"It can cause exception {nameof(IndexOutOfRangeException)}");
     }
 }
